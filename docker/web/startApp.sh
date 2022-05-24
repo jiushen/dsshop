eb59aea5aad7c3b4f74b078213d8aab43eb31ec8
leonxing(邢啸屹) <leonxing@tencent.com>
2022-05-24 17:19:52 +0800

    重新提交这几个文件，dos2unix

--- docker/web/startApp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-###### 本地开发/测试环境使用 修改本地代码后会自动重新编译 ########
-cd /src
-chmod 755 /src/node_modules/.bin/*
-/usr/local/bin/npm install
-/usr/local/bin/npm rebuild node-sass
-/usr/local/bin/npm run dev
