5b5d8de0d6fb79e7db2c7da0124bd224a7139dc8
leonxing(邢啸屹) <leonxing@tencent.com>
2022-05-24 17:38:19 +0800

    添加/usr/local/bin/npm install nuxt

--- docker/web/startApp.sh
+++ docker/web/startApp.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 ###### 本地开发/测试环境使用 修改本地代码后会自动重新编译 ########
 cd /src
-chmod 755 /src/node_modules/.bin/*
 /usr/local/bin/npm install
+/usr/local/bin/npm install nuxt
 /usr/local/bin/npm rebuild node-sass
+chmod 755 /src/node_modules/.bin/*
 /usr/local/bin/npm run dev
