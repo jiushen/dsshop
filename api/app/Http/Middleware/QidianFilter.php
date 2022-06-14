bc08be1b64ce2ff14343bf19e403ff6665bbb701
leonxing(邢啸屹) <leonxing@tencent.com>
2022-06-14 13:03:42 +0800

    支持https，另外跑通了整个发起会话授权流程

--- api/app/Http/Middleware/QidianFilter.php
+++ api/app/Http/Middleware/QidianFilter.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Middleware;
 
+use Lcobucci\JWT\Token\Parser;
+use Lcobucci\JWT\Encoding\JoseEncoder;
+use Laravel\Passport\Token;
 use Closure;
 
 class QidianFilter
@@ -15,19 +18,69 @@ class QidianFilter
      */
     public function handle($request, Closure $next)
     {
-        //xdebug_break();
+        $qidian_flag = false;
+        $user_flag = false;
         if($request->has("appid")){
+            $qidian_flag = true;
             $request->merge([
                 "grant_type"=>"client_credentials",
                 "client_id"=>$request->input("appid"),
                 "client_secret"=>$request->input("secret")
             ]);
-//            $request->except([
-//                "appid","secret"
-//            ]);
             request()->request->remove('appid');
             request()->request->remove('secret');
+        }else if ($request->has("access_token")){
+            $user_flag = true;
+
+            $token = $request->input("access_token");
+            $user_token = (new Parser(new JoseEncoder()))->parse($token)->claims()->all()['jti'];
+            $client = Token::find($user_token)->client;
+            $request->merge([
+                "grant_type"=>"authorization_code",
+                "client_id"=>$client->id,
+                "client_secret"=>$client->secret,
+                "redirect_uri"=>$client->redirect,
+            ]);//本来就带了code
+            request()->request->remove('receptionType');
         }
-        return $next($request);
+
+        $response = $next($request);
+
+        if($response->status()==200){
+            $newret['code'] = 0;
+            $newret['message'] = "success";
+
+            if($qidian_flag)
+            {
+                $ret = $response->getContent();
+                $retobj = json_decode($ret);
+                $newret['data'] = [
+                    "accessToken"=>$retobj->access_token
+                ];
+            }else if($user_flag){
+                $ret = $response->getContent();
+                $retobj = json_decode($ret);
+                $rsp_token = $retobj->access_token;
+                $user_token = (new Parser(new JoseEncoder()))->parse($rsp_token)->claims()->all()['jti'];
+                $user_id = Token::find($user_token)->user_id;
+                $user = \App\Models\v1\User::find($user_id);
+                $newret['data'] = [
+                    "openID"=>$user->uuid,
+                    "type"=>99,
+                    "nickname"=>$user->nickname,
+                    "gender"=>$user->gender,
+                    "avatar"=>$user->portrait,
+                    "email"=>$user->email,
+                    ];
+            }
+        }else{
+            $newret['code'] = $response->status();
+            $newret['message'] = $response::$statusTexts[$response->status()];
+            $newret['data'] = "";
+        }
+
+        $response->setContent(json_encode($newret));
+
+        return $response;
     }
 }
